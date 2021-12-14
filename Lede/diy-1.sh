6c14b572c3f08cebc1e50ff93e63816de24dac82
ythyty <44136048+ythyty@users.noreply.github.com>
2021-12-14 14:56:05 +0800

    Update diy-1.sh

--- Lede/diy-1.sh
+++ Lede/diy-1.sh
@@ -35,7 +35,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-oaf-ap
 # git clone https://github.com/destan19/openfros.git package/lean/luci-oaf-app  # 应用过滤插件6.0 test
 
 # # 魔改版luci-app-control-weburl，和原版相比可以多种控制模式：关键字过滤、MAC黑名单
-svn co https://github.com/ythyty/openwrt-packages-2/trunk/luci-app-control-weburl package/lean/luci-app-control-weburl
+# svn co https://github.com/ythyty/openwrt-packages-2/trunk/luci-app-control-weburl package/lean/luci-app-control-weburl
+git clone https://github.com/ythyty/luci-app-control-weburl package/lean/luci-app-control-weburl
 
 # # 全能信息推送，钉钉推送,企业微信推送,PushPlus推送,微信推送,企业微信应用推送,飞书推送,钉钉机器人推送,企业微信机器人推送,飞书机器人推送,一对多推送,Bark推送(仅iOS)
 # # git clone https://github.com/zzsj0928/luci-app-pushbot package/lean/luci-app-pushbot
