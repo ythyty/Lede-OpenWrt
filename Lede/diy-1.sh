8817e487c582879d44b0502e39c5b452bd171730
ythyty <44136048+ythyty@users.noreply.github.com>
2021-12-23 22:58:11 +0800

    Update diy-1.sh

--- Lede/diy-1.sh
+++ Lede/diy-1.sh
@@ -9,6 +9,10 @@
 # #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 
 
+# rm -rf feeds/packages/libs/libcap 
+svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap 
+
+
 #git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge-动态登陆界面
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
 
